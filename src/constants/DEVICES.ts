71c29e4cf6845b406d73781aef08db3b45618a63
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-03 22:55:38 +0300

    add media query constants

---
+++ src/constants/DEVICES.ts
@@ -0,0 +1,16 @@
+export const MOBILE_MEDIA_QUERY = {
+    min: 0,
+    max: 767,
+}
+
+export const TABLET_MEDIA_QUERY = {
+    min: 768,
+    max: 1024,
+}
+
+export const DESKTOP_MEDIA_QUERY = {
+    min: 1025,
+    max: 1920,
+}
+
+export const DEVICES_MEDIA_QUERY_LIST = [MOBILE_MEDIA_QUERY, TABLET_MEDIA_QUERY, DESKTOP_MEDIA_QUERY];
