74a89313be239c06dc99dd8f0f684239b85b2889
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-07 22:49:09 +0300

    adding outlet for transactions

--- src/pages/transactions/TransactionsPage.tsx
+++ src/pages/transactions/TransactionsPage.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const TransactionsPage = () => {
   return (
-    <div>TransactionsPage</div>
+    <div>
+        TransactionsPage
+        <Outlet />
+    </div>
   )
 }
 
