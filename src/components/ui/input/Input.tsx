17158f2e76b6dc7c5a8c32f2b64fd55fd3be86d1
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-08 23:25:18 +0300

    implement input

---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,19 @@
+import React  from 'react'
+interface Props {
+    id: string,
+    className: string,
+    type: string,
+    disabled?: boolean,
+    ref?: React.RefObject<HTMLInputElement>,
+    onClick?: () => void,
+    onChange?: () => void,
+    onFocus?: () => void,
+    onInput?: () => void,
+    onBlur?: () => void,
+}   
+
+const Input = (props: Props) => {
+  return <input {...props} />
+}
+
+export default Input
