6b57590f229e2b5a487141d384bffa9a84ad083b
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-08 23:54:03 +0300

    implement error output

---
+++ src/components/ui/input/ErrorOutput.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+interface Props {
+    className?: string
+    children: React.ReactNode,
+}
+const ErrorOutput = ({ children, ...args}:Props) => {
+  return (
+    <div {...args}>{children}</div>
+  )
+}
+
+export default ErrorOutput
