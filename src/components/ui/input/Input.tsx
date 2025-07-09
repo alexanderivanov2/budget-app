4f579f099f16cd5d6c61e5961e4455f64481cd0a
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-09 23:33:25 +0300

    adjustments on input ui components

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,17 +1,18 @@
 import React  from 'react'
 interface Props {
     id: string,
-    className: string,
+    className?: string,
     type: string,
+    name: string,
     value?: string | number,
     placeholder?: string,
     disabled?: boolean,
     ref?: React.RefObject<HTMLInputElement>,
-    onClick?: () => void,
-    onChange?: () => void,
-    onFocus?: () => void,
-    onInput?: () => void,
-    onBlur?: () => void,
+    onClick?: (e: React.MouseEvent<HTMLInputElement>) => void,
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void,
+    onInput?: (e: React.FormEvent<HTMLInputElement>) => void,
+    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void,
 }   
 
 const Input = (props: Props) => {
