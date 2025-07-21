cd5053e0786348190bc91845a0380e031462f9f9
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-22 00:05:27 +0300

    implement initial convert util file

---
+++ src/utils/convert.ts
@@ -0,0 +1,2 @@
+export const convertStringDateToDate = (value: string) => new Date(value);
+export const dateToStringValue = (value: Date) => `${value.getFullYear()}-${String(value.getMonth() + 1).padStart(2, '0')}-${String(value.getDate()).padStart(2, '0')}`
