06a14a46ff941c2765aca6278ae449d7d1b532cd
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-07-30 00:09:43 +0300

    implemnet transaction page navigation for mobile

--- src/pages/transactions/TransactionsPage.tsx
+++ src/pages/transactions/TransactionsPage.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
+import s from './TransactionPage.module.scss';
+import useDeviceMediaQuery from '../../hooks/useDeviceMediaQuery';
 
 const TransactionsPage = () => {
+  const { pathname } = useLocation();
+  const { isMobile } = useDeviceMediaQuery();
   return (
-    <div className='transactions'>
-        <Outlet />
+    <div className={s.transactions}>
+      {isMobile &&
+        <div className={s['transactions-navigation']}>
+          <Link to="income" className={`${pathname.includes('income') ? s.active : ''}`}>Income</Link>
+          <Link to="expenses" className={`${pathname.includes('expenses') ? s.active : ''}`}>Expense</Link>
+        </div>
+      }
+      <Outlet />
     </div>
   )
 }
