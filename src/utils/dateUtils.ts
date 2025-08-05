6a71bef3aa0b73c7a338c1e18364a26c1ece3e44
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-08-06 00:47:53 +0300

    implement date utils

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,12 @@
+export const getYearMonthDay = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return { year, month, day }
+}
+
+export const getDaysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate();
+
+
+export const getDaysInMonthArrayReverse = (daysCount: number) => Array.from({ length: daysCount }, (_: any, i: number) => daysCount - i);
