932e5a5d13de5895d02f8620a1e42f80af829e3a
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-08-12 23:00:16 +0300

    add new extract hook in trasnaction list

--- src/pages/statistics/components/TransactionsList.tsx
+++ src/pages/statistics/components/TransactionsList.tsx
@@ -1,41 +1,26 @@
 import { useDataContext } from "../../../context/DataContext";
-import type { Data } from "../../../context/types/DataContextTypes";
 import { getYearMonthDay } from "../../../utils/dateUtils";
-import TransactionListMonths from "./TransactionListMonths";
-
-const getAvailableYears = (year: number, data: Data) => {
-    let yearCheck = year;
-    const availableYears = [];
-    while (yearCheck >= 2000) {
-        data[yearCheck] ? availableYears.push(yearCheck) : ''
-        yearCheck--;
-    }
-
-    return availableYears;
-}
-
-const getMergeDates = (firstDates: number[], secondDate: number[]) => {
-    return Array.from(new Set([...firstDates, ...secondDate])).sort((a, b) => b - a);
-};
+import useExtractAllTransactions from "../hooks/useExtractAllTransactions";
 
 const TransactionList: React.FC = () => {
-    const { incomeData, expenseData } = useDataContext();
-    const { year, month } = getYearMonthDay(new Date());
-    const startYear = year;
-    const startMonth = month;
-    const income = getAvailableYears(year, incomeData)
-    const expense = getAvailableYears(year, expenseData)
-    const mergedYears = getMergeDates(income, expense);
+    const { transactions } = useDataContext();
+    const initialDate = getYearMonthDay(new Date());
+    const { extractedData, dateCursor, collectNewExtractData } = useExtractAllTransactions(initialDate);
+
 
     return (
         <div className="statistics-transactions-list">
             <h3>TRANSACTIONS LIST</h3>
-            {mergedYears.map((year) => {
-                return (<div key={year}>
-                    <h4>{year} </h4>
-                    <TransactionListMonths year={year} startMonth={year === startYear ? startMonth : 12} />
-                </div>)
-            })}
+            <div>
+                <p>{dateCursor.current.year}-{dateCursor.current.month}-{dateCursor.current.day}</p>
+            </div>
+            <div>
+                {extractedData.map((data, i) => <div key={data.id}>#{i} = {data.id}: {new Date(data.date).toLocaleDateString()}</div>)}
+            </div>
+            <p>{Object.keys(transactions).length} === {extractedData.length}</p>
+            {Object.keys(transactions).length === extractedData.length ? <p>'NO MORE'</p> :
+                <button onClick={collectNewExtractData}>NEW LIST</button>
+            }
         </div>
     )
 }
