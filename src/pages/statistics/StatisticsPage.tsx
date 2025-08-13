b684abfa200b28557b7dab9ed54947d8621c1f26
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-08-14 00:46:19 +0300

    implement initial transactions presentation variant

--- src/pages/statistics/StatisticsPage.tsx
+++ src/pages/statistics/StatisticsPage.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import TransactionList from './components/TransactionsList';
+import TransactionsPagination from './components/TransactionsPagination';
 
 export const StatisticsPage = () => {
+  const [transactionsVariant, setTransactionsVariant] = useState('list');
+
+  const changeTransactionsVariant = (variant: string) => {
+    setTransactionsVariant(variant);
+  }
+
   return (
-    <div className='statistics'>StatisticsPage
-      <TransactionList />
+    <div className='statistics'>
+      <h2>
+        StatisticsPage
+      </h2>
+      <div style={{ display: 'flex'}}> <button onClick={() => changeTransactionsVariant('list')}>List</button> <button onClick={() => changeTransactionsVariant('pagination')}>Pagination</button></div>
+      { transactionsVariant === 'list' ? <TransactionList /> : <TransactionsPagination /> }
     </div>
   )
 }
---
+++ src/pages/statistics/components/TransactionsPagination.tsx
@@ -0,0 +1,7 @@
+const TransactionsPagination: React.FC = () => {
+    return (<div>
+        <h2>PAGINATION LIST</h2>
+        </div>)
+}
+
+export default TransactionsPagination;
