091caa2b58125bb4d2701a7ad00fae8cd732c64d
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-09-08 17:57:46 +0300

    add and configurate prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** {import('prettier').Config} */
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    endOfLine: 'lf',
+};
