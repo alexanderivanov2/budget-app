1c3581a655d5e30da5fbffee42889e2a26dfcc7a
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-09-16 00:07:16 +0300

    implement initial day month year change

---
+++ src/features/timeframe/components/TimeFrameNavigator.tsx
@@ -0,0 +1,147 @@
+import { useState, type ChangeEvent } from 'react';
+import { useTimeFrameContext } from './TimeFrameContext';
+
+const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+const TimeFrameNavigator: React.FC = () => {
+    const [navigatorDate, setNavigatorDate] = useState(() => new Date());
+    const { timeFrameType } = useTimeFrameContext();
+    if (timeFrameType === 'allTime') return null;
+
+    const getNavigatorInputData = () => {
+        switch (timeFrameType) {
+            case 'year': {
+                return {
+                    inputType: 'year',
+                    inputValue: navigatorDate.getFullYear(),
+                };
+            }
+            case 'month': {
+                const month = navigatorDate.getMonth() || 0;
+                const monthValue = MONTHS[month];
+                return {
+                    inputType: 'month',
+                    inputValue: monthValue,
+                };
+            }
+            case 'day': {
+                return {
+                    inputType: 'day',
+                    inputValue: navigatorDate.getDate(),
+                };
+            }
+            default: {
+                return {
+                    inputType: 'year',
+                    inputValue: navigatorDate.getFullYear(),
+                };
+            }
+        }
+    };
+
+    const handleChangeNavigatorInputDate = (e: ChangeEvent) => {
+        console.log(e.currentTarget);
+    };
+
+    const prevYear = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            dateUpdate.setFullYear(dateUpdate.getFullYear() - 1);
+            return dateUpdate;
+        });
+    };
+
+    const prevMonth = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            const monthValue = dateUpdate.getMonth();
+            const newMonth = monthValue ? monthValue - 1 : 11;
+            dateUpdate.setMonth(newMonth);
+            return dateUpdate;
+        });
+    };
+
+    const prevDay = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            const dayValue = dateUpdate.getDate() - 1;
+
+            // use Date overflow - 1;
+            dateUpdate.setDate(dayValue);
+            return dateUpdate;
+        });
+    };
+
+    const nextDay = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            const dayValue = dateUpdate.getDate() + 1;
+
+            // use Date overflow + 1
+            dateUpdate.setDate(dayValue);
+            return dateUpdate;
+        });
+    };
+
+    const nextMonth = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            const monthValue = dateUpdate.getMonth();
+            const newMonth = monthValue === 11 ? 0 : monthValue + 1;
+            dateUpdate.setMonth(newMonth);
+            return dateUpdate;
+        });
+    };
+
+    const nextYear = () => {
+        setNavigatorDate((prevDate) => {
+            const dateUpdate = new Date(prevDate);
+            dateUpdate.setFullYear(dateUpdate.getFullYear() + 1);
+            return dateUpdate;
+        });
+    };
+
+    const handlePrevChangeTimeFrameNavigator = () => {
+        if (timeFrameType === 'year') {
+            prevYear();
+        } else if (timeFrameType === 'month') {
+            prevMonth();
+        } else if (timeFrameType === 'day') {
+            prevDay();
+        }
+    };
+
+    const handleNextChangeTimeFrameNavigator = () => {
+        if (timeFrameType === 'year') {
+            nextYear();
+        } else if (timeFrameType === 'month') {
+            nextMonth();
+        } else if (timeFrameType === 'day') {
+            nextDay();
+        }
+    };
+    const { inputType, inputValue } = getNavigatorInputData();
+
+    return (
+        <div className="time-frame-navigator">
+            <p>
+                <span>YEAR: {navigatorDate.getFullYear()} </span>
+                <span>MONTH: {MONTHS[navigatorDate.getMonth()]} </span>
+                <span>DAY: {navigatorDate.getDate()} </span>
+            </p>
+            <div className="time-frame-navigator-prev" onClick={handlePrevChangeTimeFrameNavigator}>
+                prev
+            </div>
+            <div className="time-frame-navigator-current-input">
+                <div>
+                    ({timeFrameType}: {inputValue}){' '}
+                </div>
+            </div>
+            <div className="time-frame-navigator-next" onClick={handleNextChangeTimeFrameNavigator}>
+                next
+            </div>
+        </div>
+    );
+};
+
+export default TimeFrameNavigator;
